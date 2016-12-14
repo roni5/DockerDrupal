e06b7a0a47fdfae34a4846066598f10783158f2a
Joe Mewes <joe@4alldigital.com>
2016-12-14 10:54:26 +0000

    refactoring / simplified config / integration with CLI

--- settings/docker.aliases.drushrc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$aliases['drupal_docker'] = array (
-  'uri' => 'http://default',
-  'root' => '/docker/drupal_docker',
-  'remote-host' => 'drupal.docker',
-  'remote-user' => 'root',
-  'ssh-options' => '-p 8022', // optional if required
-);
